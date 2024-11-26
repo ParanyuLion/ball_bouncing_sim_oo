1e7f519b92854e46a30daeed1d9f5a4e890df5d7
Paranyu KITTINAVAKIT <paranyu.lion@gmail.com>
2024-11-26 23:34:15 +0700

    second commit

--- ball.py
+++ ball.py
@@ -1,6 +1,7 @@
 import turtle
 import math
 
+
 class Ball:
     def __init__(self, size, x, y, vx, vy, color, id):
         self.size = size
@@ -9,19 +10,18 @@ class Ball:
         self.vx = vx
         self.vy = vy
         self.color = color
-        self.mass = 100*size**2
+        self.mass = 100 * size ** 2
         self.count = 0
         self.id = id
-        self.canvas_width = turtle.screensize()[0]
-        self.canvas_height = turtle.screensize()[1]
-        
+        self.canvas_width = 550
+        self.canvas_height = 350
 
     def draw(self):
         # draw a circle of radius equals to size centered at (x, y) and paint it with color
         turtle.penup()
         turtle.color(self.color)
         turtle.fillcolor(self.color)
-        turtle.goto(self.x, self.y-self.size)
+        turtle.goto(self.x, self.y - self.size)
         turtle.pendown()
         turtle.begin_fill()
         turtle.circle(self.size)
@@ -36,12 +36,12 @@ class Ball:
         self.count += 1
 
     def bounce_off(self, that):
-        dx  = that.x - self.x
-        dy  = that.y - self.y
+        dx = that.x - self.x
+        dy = that.y - self.y
         dvx = that.vx - self.vx
         dvy = that.vy - self.vy
-        dvdr = dx*dvx + dy*dvy; # dv dot dr
-        dist = self.size + that.size   # distance between particle centers at collison
+        dvdr = dx * dvx + dy * dvy;  # dv dot dr
+        dist = self.size + that.size  # distance between particle centers at collison
 
         # magnitude of normal force
         magnitude = 2 * self.mass * that.mass * dvdr / ((self.mass + that.mass) * dist)
@@ -55,7 +55,7 @@ class Ball:
         self.vy += fy / self.mass
         that.vx -= fx / that.mass
         that.vy -= fy / that.mass
-        
+
         # update collision counts
         self.count += 1
         that.count += 1
@@ -65,31 +65,31 @@ class Ball:
         y1 = self.y
         x2 = that.x
         y2 = that.y
-        d = math.sqrt((y2-y1)**2 + (x2-x1)**2)
+        d = math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
         return d
 
     def move(self, dt):
-        self.x += self.vx*dt
-        self.y += self.vy*dt
+        self.x += self.vx * dt
+        self.y += self.vy * dt
 
     def time_to_hit(self, that):
         if self is that:
             return math.inf
-        dx  = that.x - self.x
-        dy  = that.y - self.y
+        dx = that.x - self.x
+        dy = that.y - self.y
         dvx = that.vx - self.vx
         dvy = that.vy - self.vy
-        dvdr = dx*dvx + dy*dvy
+        dvdr = dx * dvx + dy * dvy
         if dvdr > 0:
             return math.inf
-        dvdv = dvx*dvx + dvy*dvy
+        dvdv = dvx * dvx + dvy * dvy
         if dvdv == 0:
             return math.inf
-        drdr = dx*dx + dy*dy
+        drdr = dx * dx + dy * dy
         sigma = self.size + that.size
-        d = (dvdr*dvdr) - dvdv * (drdr - sigma*sigma)
+        d = (dvdr * dvdr) - dvdv * (drdr - sigma * sigma)
         # if drdr < sigma*sigma:
-            # print("overlapping particles")
+        # print("overlapping particles")
         if d < 0:
             return math.inf
         t = -(dvdr + math.sqrt(d)) / dvdv
@@ -118,15 +118,15 @@ class Ball:
             return math.inf
 
     def time_to_hit_paddle(self, paddle):
-        if (self.vy > 0) and ((self.y + self.size) > (paddle.location[1] - paddle.height/2)):
+        if (self.vy > 0) and ((self.y + self.size) > (paddle.location[1] - paddle.height / 2)):
             return math.inf
-        if (self.vy < 0) and ((self.y - self.size) < (paddle.location[1] + paddle.height/2)):
+        if (self.vy < 0) and ((self.y - self.size) < (paddle.location[1] + paddle.height / 2)):
             return math.inf
 
-        dt = (math.sqrt((paddle.location[1] - self.y)**2) - self.size - paddle.height/2) / abs(self.vy)
-        paddle_left_edge = paddle.location[0] - paddle.width/2
-        paddle_right_edge = paddle.location[0] + paddle.width/2
-        if paddle_left_edge - self.size <= self.x + (self.vx*dt) <= paddle_right_edge + self.size:
+        dt = (math.sqrt((paddle.location[1] - self.y) ** 2) - self.size - paddle.height / 2) / abs(self.vy)
+        paddle_left_edge = paddle.location[0] - paddle.width / 2
+        paddle_right_edge = paddle.location[0] + paddle.width / 2
+        if paddle_left_edge - self.size <= self.x + (self.vx * dt) <= paddle_right_edge + self.size:
             return dt
         else:
             return math.inf
@@ -136,4 +136,5 @@ class Ball:
         self.count += 1
 
     def __str__(self):
-        return str(self.x) + ":" + str(self.y) + ":" + str(self.vx) + ":" + str(self.vy) + ":" + str(self.count) + str(self.id)
+        return str(self.x) + ":" + str(self.y) + ":" + str(self.vx) + ":" + str(self.vy) + ":" + str(self.count) + str(
+            self.id)
--- run_ball.py
+++ run_ball.py
@@ -1,9 +1,15 @@
+import copy
 import ball
 import my_event
 import turtle
 import random
 import heapq
 import paddle
+import time
+import math
+import enemies
+import os
+import winsound
 
 class BouncingSimulator:
     def __init__(self, num_balls):
@@ -16,24 +22,54 @@ class BouncingSimulator:
         turtle.tracer(0)
         turtle.hideturtle()
         turtle.colormode(255)
-        self.canvas_width = turtle.screensize()[0]
-        self.canvas_height = turtle.screensize()[1]
+        self.canvas_width = 550
+        self.canvas_height = 350
+        self.shot_time = 0
+        self.time = time.time()
+        self.enemies_move_time = 0
+        self.enemies = []
+        self.score = 0
         print(self.canvas_width, self.canvas_height)
 
-        ball_radius = 0.05 * self.canvas_width
+        ball_radius = 50
         for i in range(self.num_balls):
             x = -self.canvas_width + (i+1)*(2*self.canvas_width/(self.num_balls+1))
-            y = 0.0
-            vx = 10*random.uniform(-1.0, 1.0)
-            vy = 10*random.uniform(-1.0, 1.0)
+            # y = 0
+            # vx = 10*random.uniform(-1.0, 1.0)
+            # vy = 10*random.uniform(-1.0, 1.0)
+            y = 350
+            vx = 0
+            vy = -1
             ball_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
             self.ball_list.append(ball.Ball(ball_radius, x, y, vx, vy, ball_color, i))
 
         tom = turtle.Turtle()
         self.my_paddle = paddle.Paddle(200, 50, (255, 0, 0), tom)
-        self.my_paddle.set_location([0, -50])
+        self.my_paddle.set_location([0, -150])
+        for i in range(3):
+            self.enemies.append((enemies.Enemies(50, random.randint(-400, 400), 300, 0, 10, 'green')))
 
+        # self.enemies1 = (enemies.Enemies(50, 0, 300, 0, 10, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))))
+        # self.enemies1.draw()
+        for i in self.enemies:
+            i.draw()
+        self.jerry = turtle.Turtle()
+        self.__draw_scores(self.jerry)
         self.screen = turtle.Screen()
+
+    def kill_enemy(self):
+        for j in range(len(self.enemies)):
+            for i in range(len(self.ball_list)):
+                if abs(self.ball_list[i].x - self.enemies[j].x) < 50 and (abs(self.ball_list[i].y - self.enemies[j].y)< 50):
+                    self.enemies[j].y = 700
+                    self.enemies[j].x = random.randint(-400, 400)
+                    self.score += 1
+                    self.__draw_scores(self.jerry)
+        # for i in range(len(self.ball_list)):
+        #     if abs(self.ball_list[i].x - self.enemies1.x) < 50 and (abs(self.ball_list[i].y - self.enemies1.y)< 50):
+        #         self.enemies1.y = 450
+        #         self.score += 1
+        #         self.__draw_scores(self.jerry)
 
     # updates priority queue with all new events for a_ball
     def __predict(self, a_ball):
@@ -45,34 +81,47 @@ class BouncingSimulator:
             dt = a_ball.time_to_hit(self.ball_list[i])
             # insert this event into pq
             heapq.heappush(self.pq, my_event.Event(self.t + dt, a_ball, self.ball_list[i], None))
-        
+
         # particle-wall collisions
         dtX = a_ball.time_to_hit_vertical_wall()
-        dtY = a_ball.time_to_hit_horizontal_wall()
+        # dtY = a_ball.time_to_hit_horizontal_wall()
         heapq.heappush(self.pq, my_event.Event(self.t + dtX, a_ball, None, None))
-        heapq.heappush(self.pq, my_event.Event(self.t + dtY, None, a_ball, None))
-    
+        # heapq.heappush(self.pq, my_event.Event(self.t + dtY, None, a_ball, None))
+
     def __draw_border(self):
         turtle.penup()
         turtle.goto(-self.canvas_width, -self.canvas_height)
         turtle.pensize(10)
         turtle.pendown()
-        turtle.color((0, 0, 0))   
+        turtle.color((0, 0, 0))
         for i in range(2):
-            turtle.forward(2*self.canvas_width)
+            turtle.forward(2 * self.canvas_width)
             turtle.left(90)
-            turtle.forward(2*self.canvas_height)
+            turtle.forward(2 * self.canvas_height)
             turtle.left(90)
+
+    def __draw_scores(self, score):
+        score.color("red")
+        style = ("Comic Sans", 30, "normal")
+        score.penup()
+        score.goto(-500, 300)
+        score.clear()
+        score.write(f"Points:{self.score} ", font=style)
+        score.hideturtle()
 
     def __redraw(self):
         turtle.clear()
         self.my_paddle.clear()
         self.__draw_border()
         self.my_paddle.draw()
+        # self.enemies1.draw()
+
+        for i in self.enemies:
+            i.draw()
         for i in range(len(self.ball_list)):
             self.ball_list[i].draw()
         turtle.update()
-        heapq.heappush(self.pq, my_event.Event(self.t + 1.0/self.HZ, None, None, None))
+        heapq.heappush(self.pq, my_event.Event(self.t + 1.0 / self.HZ, None, None, None))
 
     def __paddle_predict(self):
         for i in range(len(self.ball_list)):
@@ -90,6 +139,74 @@ class BouncingSimulator:
         if (self.my_paddle.location[0] + self.my_paddle.width/2 + 40) <= self.canvas_width:
             self.my_paddle.set_location([self.my_paddle.location[0] + 40, self.my_paddle.location[1]])
 
+    def move_up(self):
+        if (self.my_paddle.location[1] + self.my_paddle.height / 2 + 40) <= self.canvas_height:
+            self.my_paddle.set_location([self.my_paddle.location[0], self.my_paddle.location[1] + 40])
+
+    def move_down(self):
+        if (self.my_paddle.location[1] - self.my_paddle.height / 2 - 40) >= -self.canvas_height:
+            self.my_paddle.set_location([self.my_paddle.location[0], self.my_paddle.location[1] - 40])
+
+    def shoot_ball(self, mouse_x=None, mouse_y=None):
+        turtle.update()
+        current_time = time.time()
+        speed = 40
+        if current_time - self.shot_time > 0.5:
+            winsound.PlaySound("gun-gunshot-01.wav", winsound.SND_ASYNC)
+            ball_radius = 10
+            x = self.my_paddle.location[0]
+            y = self.my_paddle.location[1] + self.my_paddle.height / 2 + ball_radius
+            # tan_v = math.sqrt(mouse_y ** 2 / mouse_x ** 2)
+            if mouse_x == x:  # Handle vertical shooting (x == x)
+                if mouse_y != y:
+                    zeta = math.pi / 2
+                else:
+                    zeta = -math.pi / 2
+            else:
+                zeta = math.atan((mouse_y - y) / (mouse_x - x))  # Calculate angle
+
+            if mouse_x < x:
+                vx = -(math.cos(zeta)) * speed
+                vy = -(math.sin(zeta)) * speed
+            else:
+                vx = (math.cos(zeta))*speed
+                vy = (math.sin(zeta))*speed
+            ball_color = (255, 0, 0)
+            ammo = ball.Ball(ball_radius, x, y, vx, vy, ball_color, len(self.ball_list))
+            # self.ball_list.append(ammo)
+            # if len(self.ball_list) > 15:
+            #     # self.ball_list[0].vx = 0
+            #     # self.ball_list[0].vy = 0
+            #     # self.ball_list[0] = ammo
+            #     # self.__predict(ammo)
+            #     self.ball_list.pop(0)
+            # else:
+            #     self.ball_list.append(ammo)
+            #     self.__predict(ammo)
+            self.ball_list.append(ammo)
+            self.__predict(ammo)
+            self.enemies_move()
+            self.shot_time = current_time
+
+    def enemies_move(self):
+        current_time = time.time()
+        if current_time - self.enemies_move_time > abs(0.2 /(1+ 0.1*self.score)):
+            for i in self.enemies:
+                i.y -= 20
+                if i.y < -400:
+                    i.y = 700
+                self.enemies_move_time = current_time
+                self.kill_enemy()
+            # self.enemies1.x = 0
+            # self.enemies1.y -= 20
+            # self.enemies1.draw()
+            # self.enemies_move_time = current_time
+            # self.kill_enemy()
+
+
+    def parry(self):
+        pass
+
     def run(self):
         # initialize pq with collision events and redraw event
         for i in range(len(self.ball_list)):
@@ -98,10 +215,17 @@ class BouncingSimulator:
 
         # listen to keyboard events and activate move_left and move_right handlers accordingly
         self.screen.listen()
-        self.screen.onkey(self.move_left, "Left")
-        self.screen.onkey(self.move_right, "Right")
+        self.screen.onkeypress(self.move_left, "a")
+        self.screen.onkeypress(self.move_right, "d")
+        self.screen.onkeypress(self.move_up, "w")
+        self.screen.onkeypress(self.move_down, "s")
+        self.screen.onkeypress(self.parry, "space")
+        self.screen.onclick(self.shoot_ball)
+
+
 
         while (True):
+
             e = heapq.heappop(self.pq)
             if not e.is_valid():
                 continue
@@ -109,7 +233,7 @@ class BouncingSimulator:
             ball_a = e.a
             ball_b = e.b
             paddle_a = e.paddle
-
+            self.enemies_move()
             # update positions, and then simulation clock
             for i in range(len(self.ball_list)):
                 self.ball_list[i].move(e.time - self.t)
@@ -131,12 +255,10 @@ class BouncingSimulator:
 
             # regularly update the prediction for the paddle as its position may always be changing due to keyboard events
             self.__paddle_predict()
-
-
-        # hold the window; close it by clicking the window close 'x' mark
+            # hold the window; close it by clicking the window close 'x' mark
         turtle.done()
 
 # num_balls = int(input("Number of balls to simulate: "))
-num_balls = 10
+num_balls = 0
 my_simulator = BouncingSimulator(num_balls)
 my_simulator.run()
